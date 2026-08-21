f5929b96765d558e20b63e753477bf225d72d459
tamayojonell <jonelltamayo3@gmail.com>
2026-08-21 16:27:51 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,3 @@
+#this is my first code in python
+
+print("Hello World!")
---
+++ activity3.py
@@ -0,0 +1,42 @@
+#using variables
+
+
+#integer
+age = 18
+day = 26
+year = 2007
+height = 154
+
+#string
+Name = "Jonell"
+Month = "December"
+Hobbie = "Sleep"
+address = "Mayao Crossing, Lucena City"
+
+#float
+weight = 47.2
+grade = 95.3
+average = 94.4
+BMI = 19.9
+
+#boolean
+isstudent = True
+isnormalBMI = True
+isteenager = True
+islazy = False
+
+print("Hello! My name is", Name,".")
+print("I am", age, "years old.")
+print("I was born on", Month, day, year,".")
+print("I am", height, "cm tall and my weight is", weight, "kg.")
+print("I live in", address,".")
+print("My hobby is", Hobbie,".")
+print("My Senior High School grade was", grade,".")
+print("The average I am afraid of getting is", average,".")
+print("My BMI is", BMI,".")
+print("I am a student:", isstudent)
+print("My BMI is normal:", isnormalBMI)
+print("I am a teenager:", isteenager)
+print("Am I lazy?", islazy)
+
+
