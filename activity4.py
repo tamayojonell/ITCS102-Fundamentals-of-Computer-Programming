01b14cbcc0838e31263ff91666dc443c8586684a
tamayojonell <jonelltamayo3@gmail.com>
2026-08-25 20:15:09 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,11 @@
+#ecscape
+# \n
+# \t
+# \r
+# \b
+# \\
+# \"
+
+phrase1 = "\tReminder: \n\t\t\"You are enough. You have nothing to prove to anybody.\" \n\t\t - Maya Angelou"
+
+print(phrase1) 
---
+++ code_challenge1.py
@@ -0,0 +1,16 @@
+#escape sequence
+
+# \n
+# \t
+# \r
+# \b
+# \\
+# \"
+
+phrase1 = "\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*"
+
+phrase2 = "\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\tHi!My Name is Jonell\t\t\t\t**\n\t\t\t\t\t**\t\t\t\tI'm from BSIT-1B 0_0\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**\n\t\t\t\t\t**\t\t\t\t\t\t\t\t\t\t**"
+
+phrase3 = "\n\t\t\t\t\t**********************************************************************************\n\t\t\t\t\t**********************************************************************************"
+
+print(phrase1, phrase2, phrase3)
