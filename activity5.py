8de8de4a38cf371d1593b954d75dc10e201a5a75
tamayojonell <jonelltamayo3@gmail.com>
2026-08-28 22:19:47 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,12 @@
+#operations activity
+
+a = 18
+b = 7
+
+print("The sum of", a, "and", b, "is", a+b)
+print("The difference of", a, "and", b, "is", a-b)
+print("The product of", a, "and", b, "is", a*b)
+print("The quotient of", a, "and", b, "is", a/b)
+print("The exponent of", a, "and", b, "is", a**b)
+print("The whole number of", a, "and", b, "is", a//b)
+print("The remainder of", a, "and", b, "is", a%b)
---
+++ code_challenge2.py
@@ -0,0 +1,12 @@
+money = input("Amount to be deposite:")
+
+print("The amount of money receivedd:", money)
+print("1000:", int(money)//1000)
+print("500:", int(money)%1000//500)
+print("200:", int(money)%1000%500//200)
+print("100:", int(money)%1000%500%200//100)
+print("50:", int(money)%1000%500%200%100//50)
+print("20:", int(money)%1000%500%200%100%50//20)
+print("10:", int(money)%1000%500%200%100%50%20//10)
+print("5:", int(money)%1000%500%200%200%100%50%20%10//5)
+print("1:", int(money)%1000%500%200%200%100%50%20%10%5//1)
