49c682c32944f6a7303f62bdd64cfa92f5f59a83
tamayojonell <jonelltamayo3@gmail.com>
2026-09-02 11:13:43 +0800

    Refactor money calculation and add user input

--- code_challenge2.py
+++ code_challenge2.py
@@ -10,3 +10,18 @@ print("20:", int(money)%1000%500%200%100%50//20)
 print("10:", int(money)%1000%500%200%100%50%20//10)
 print("5:", int(money)%1000%500%200%200%100%50%20%10//5)
 print("1:", int(money)%1000%500%200%200%100%50%20%10%5//1)
+
+
+money = input("To be deposite:")
+
+print("The amount of money received:", money)
+print("1000:", int(money)//1000)
+print("500:", int(money)%1000//500)
+print("200:", int(money)%1000%500//200)
+print("100:", int(money)%1000%500%200//100)
+print("50:", int(money)%1000%500%200%100//50)
+print("20:", int(money)%1000%500%200%100%50//20)
+print("10:", int(money)%1000%500%200%100%50%20//10)
+print("5:", int(money)%1000%500%200%200%100%50%20%10//5)
+print("1:", int(money)%1000%500%200%200%100%50%20%10%5//1)
+
